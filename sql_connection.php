c08f5ca6f4e118e3333914270500271a6a2c79fb
kohdaikuro222 <kohdaikuro222@gmail.com>
2018-10-10 14:02:05 -0400

    Add files via upload

--- sql_connection.php
+++ sql_connection.php
@@ -8,7 +8,15 @@ function db_connect(){
 	// password
     $password = 'kohdai0621';
     
-    $dbh = new PDO($dsn, $user, $password);
+    try {
+		$dbh = new PDO($dsn, $user, $password);
+		// set the PDO error mode to exception
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+	catch(PDOException $e)
+		{
+		echo "Connection failed: " . $e->getMessage();
+		}
 	return $dbh;
 }
  
--- test_mysql.php
+++ test_mysql.php
@@ -13,6 +13,16 @@
 <div>
 <!--Create add Button -->
 <p id = add><a href = "./test_add.html"> Add Data </a></p>
+<form action="test_mysql.php" method="post">
+<input type="radio" name="Check" value="ID">ID
+<input type="radio" name="Check" value="Name">Name
+<input type="radio" name="Check" value="Price">Price
+<input type="radio" name="Check" value="Detail">Detail
+<input type="text" name="Value" value="">
+<input type="submit" value="Filter">
+</form>
+<p id = undo><a href = "./test_mysql.php"> undo </a></p>
+
     <div class = "Head">
         <table>
             <thead>
@@ -34,14 +44,47 @@
     //Connect to the database
     require_once("sql_connection.php");
     $pdo = db_connect();
+    if(empty($_POST['Check'])){
+        $stmt = $pdo->prepare("SELECT * FROM testtable");
+        $stmt -> execute();
+    }else if($_POST['Check'] === 'ID'){
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE ID = :ID");
+        $stmt->bindValue(':ID', $_POST['Value'], PDO::PARAM_INT);
+        $stmt->execute();
+    }else if($_POST['Check'] === 'Name'){
+        $Array_char = str_split($_POST['Value']);
+        $search_str = "%";
+        foreach($Array_char as $value){
+            $search_str = $search_str . $value . "%";
+        }
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE name like :Name");
+        $stmt->bindValue(':Name', $search_str, PDO::PARAM_STR);
+        $stmt->execute();
+    }else if($_POST['Check'] === 'Price'){
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE price = :Price");
+        $stmt->bindValue(':Price', $_POST['Value'], PDO::PARAM_INT);
+        $stmt->execute();
+    }else if($_POST['Check'] === 'Detail'){
+        $Array_char = str_split($_POST['Value']);
+        $search_str = "%";
+        foreach($Array_char as $value){
+            $search_str = $search_str . $value . "%";
+        }
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE detail like :Detail");
+        $stmt->bindValue(':Detail', $search_str, PDO::PARAM_STR);
+        $stmt->execute();
+    }else{
+        $stmt = $pdo->prepare("SELECT * FROM testtable");
+        $stmt -> execute(); 
+    }
     //Take data from the database by using SQL
-    foreach ( $pdo->query ( 'select * from testtable' ) as $row ) {
+    foreach ( $stmt as $row ) {
     ?>
-        <div class = Line>
+        <div>
         <table>
             <!--Body of the table -->
             <tbody>
-                <tr>
+                <tr  class = Line>
                     <td class = "column1"><?=$row['ID']?></td>
                     <td class = "column2"><?=$row['name']?></td>
                     <td class = "column3"><?=$row['price']?></td>
@@ -77,6 +120,5 @@
     ?>
     </div>
 </div>
-
 </body>
 </html>
