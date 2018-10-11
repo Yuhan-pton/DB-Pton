363d7295a621a0526d70a70106b880f642fd9624
kohdaikuro222 <kohdaikuro222@gmail.com>
2018-10-11 16:08:00 -0400

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<div id="sidenav">
+  <a href="test_mysql.php">Home</a>
+  <a href="https://phy-wiki.princeton.edu/so-pton/pmwiki.php?n=Main.HomePage">Wiki</a>
+  <a href="http://128.112.85.137/Equipment_Cost.php">Equipment Cost</a>
+</div>
+
--- test_mysql.php
+++ test_mysql.php
@@ -3,48 +3,128 @@
 <head>
 <title>Database</title>
 <meta charset="utf-8">
+<!-- for desigh-->
+<link rel="stylesheet" type="text/css" href="testdb.css">
+<link rel="stylesheet" type="text/css" href="util.css">
+<!--------------->
 </head>
-<style>
-#add a {
-    color : white;
-    background : skyblue;
-    width : 100px;
-    height : 20px;
-    text-decoration : none
-}
-</style>
 <body>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Detail</th>
-    </tr>
-<?php
-echo '<h1>', "Database" ,'</h1>';
-$pdo = new PDO ( 'mysql:host=127.0.0.1;port=3308;dbname=testdata;charset=utf8', 'root', 'kohdai0621' );
-foreach ( $pdo->query ( 'select * from testtable' ) as $row ) {
-?>
-    <tr>
-    <td class = "elements"><?=$row['ID']?></td>
-    <td class = "elements"><?=$row['name']?></td>
-    <td class = "elements"><?=$row['price']?></td>
-    <td class = "elements"><?=$row['detail']?></td>
-    <td>
-		<form action="test_delete.php" method="post">
-		<input type="submit" value="Delete">
-        <input type="hidden" name="ID" value="<?=$row['ID']?>">
-        <input type="hidden" name="Name" value="<?=$row['name']?>">
-        <input type="hidden" name="Price" value="<?=$row['price']?>">
-        <input type="hidden" name="Detail" value="<?=$row['detail']?>">
-		</form>
-	</td>
-    </tr>
-<?php
-}
-?>
-</table>
+<div>
+    <?php //include( $_SERVER['DOCUMENT_ROOT'] . './test_database/sidebar.php'); ?>  
+</div>
+<h1>Database</h1>
+<!--Create add Button -->
+<div>
 <p id = add><a href = "./test_add.html"> Add Data </a></p>
+<form action="test_mysql.php" method="post" id=filter >
+<input type="radio" name="Check" value="ID">ID
+<input type="radio" name="Check" value="Name">Name
+<input type="radio" name="Check" value="Price">Price
+<input type="radio" name="Check" value="Detail">Detail
+<input type="text" name="Value" value="">
+<input type="submit" value="Filter">
+<input type="button" value="Undo" onclick = "location.href='./test_mysql.php'">
+</form>
+</div>
+
+    <div class = "Head">
+        <table align = "right">
+            <thead>
+                <!--Create a head of the table -->
+                <tr>
+                    <th class = "column1">ID</th>
+                    <th class = "column2">Name</th>
+                    <th class = "column3">Price</th>
+                    <th class = "column4">Detail</th>
+                    <th class = "column5">Edit</th>
+                    <th class = "column6">Delete</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    <div>
+    <?php
+    header("Content-type: text/html; charset=utf-8");
+    //Connect to the database
+    require_once("sql_connection.php");
+    $pdo = db_connect();
+    if(empty($_POST['Check'])or((!empty($_POST['Check']))&&(($_POST['Value'])==""))){
+        $stmt = $pdo->prepare("SELECT * FROM testtable");
+        $stmt -> execute();
+    }else if($_POST['Check'] === 'ID'){
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE ID = :ID");
+        $stmt->bindValue(':ID', $_POST['Value'], PDO::PARAM_INT);
+        $stmt->execute();
+    }else if($_POST['Check'] === 'Name'){
+        $Array_char = str_split($_POST['Value']);
+        $search_str = "%";
+        foreach($Array_char as $value){
+            $search_str = $search_str . $value . "%";
+        }
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE name like :Name");
+        $stmt->bindValue(':Name', $search_str, PDO::PARAM_STR);
+        $stmt->execute();
+    }else if($_POST['Check'] === 'Price'){
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE price = :Price");
+        $stmt->bindValue(':Price', $_POST['Value'], PDO::PARAM_INT);
+        $stmt->execute();
+    }else if($_POST['Check'] === 'Detail'){
+        $Array_char = explode(" ", $_POST['Value']);
+        $search_str = "%";
+        foreach($Array_char as $value){
+            $search_str = $search_str . $value . "%";
+        }
+        $stmt = $pdo->prepare("SELECT * FROM testtable WHERE detail like :Detail");
+        $stmt->bindValue(':Detail', $search_str, PDO::PARAM_STR);
+        $stmt->execute();
+    }else{
+        $stmt = $pdo->prepare("SELECT * FROM testtable");
+        $stmt -> execute(); 
+    }
+    //Take data from the database by using SQL
+    foreach ( $stmt as $row ) {
+    ?>
+        <div>
+        <table align = "right">
+            <!--Body of the table -->
+            <tbody>
+                <tr  class = Line>
+                    <td class = "column1" ><?=$row['ID']?></td>
+                    <td class = "column2"><?=$row['name']?></td>
+                    <td class = "column3"><?=$row['price']?></td>
+                    <td class = "column4"><?=$row['detail']?></td>
+                    <!-- Edit Button-->
+                    <td class = "column5">
+                        <!-- post the following data to "test_edit.php" when this button is pushed-->
+                        <form action="test_edit.php" method="post">
+                        <input id="edit" type="submit" value="Edit">
+                        <input type="hidden" name="ID" value="<?=$row['ID']?>">
+                        <input type="hidden" name="Name" value="<?=$row['name']?>">
+                        <input type="hidden" name="Price" value="<?=$row['price']?>">
+                        <input type="hidden" name="Detail" value="<?=$row['detail']?>">
+                        </form>
+                    </td>
+                    <!-- Delete Button-->
+                    <td class="column6">
+                        <!-- post the following data to "password.php" when this button is pushed-->
+                        <form action="password.php" method="post">
+                        <input id="delete" type="submit" value="Delete">
+                        <input type="hidden" name="ID" value="<?=$row['ID']?>">
+                        <input type="hidden" name="Name" value="<?=$row['name']?>">
+                        <input type="hidden" name="Price" value="<?=$row['price']?>">
+                        <input type="hidden" name="Detail" value="<?=$row['detail']?>">
+                        </form>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <?php
+    }
+    ?>
+    </div>
+
+    <img src="./picture.jpg" width="200px" height="200px" alt="picture">
+
 </body>
 </html>
