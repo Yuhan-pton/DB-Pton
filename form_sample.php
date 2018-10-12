aaefdde3cb8a2f49398f25183581bcc6f1a53300
kohdaikuro222 <kohdaikuro222@gmail.com>
2018-10-12 12:26:30 -0400

    Sample code for "submit form"

---
+++ form_sample.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!-- Sample code -->
+<html>
+<body>
+  <form action="./form_sample.php" method="post">
+    <!-- textbox-->
+    <input type="text" value="Sample"><br>
+    <!-- password form-->
+    <input type="password" value="Sample"><br>
+    <!-- radio button-->
+    <input type="radio" name="Radio" value="1">1
+    <input type="radio" name="Radio" value="2">2
+    <input type="radio" name="Radio" value="3">3<br>
+    <!-- checkbox-->
+    <input type="checkbox" name="check" value="1">1
+    <input type="checkbox" name="check" value="2">2
+    <input type="checkbox" name="check" value="3">3<br>
+    <!-- select box-->
+    <select name="example">
+    <option value="sample1">sample1</option>
+    <option value="sample2">sample2</option>
+    <option value="sample3">sample3</option>
+    </select><br>
+    <!-- file -->
+    <input type="file" name="file" value=""><br>
+    <!--hidden form-->
+    <input type="hidden" name=""hidden value="Sample"><br>
+    <!-- submit button-->
+    <input type="submit" name="submit" value = "submit">
+   </form>
+  </body>
+ </html>
